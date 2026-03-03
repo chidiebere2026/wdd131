db360270a125663117b31727071193934e84d74e
chidiebere2026 <asoyamary@gmail.com>
2026-03-03 11:37:28 +0100

    First init

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Dynamic Copyright Year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Last Modified Date
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
