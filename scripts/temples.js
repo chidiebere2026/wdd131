d276b85fa3d9db1b4b4f8356c1b7065dbfb26e17
praizarts <asoyamary@gmail.com>
2026-03-07 10:05:40 +0100

    added the week 2 temple activity

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,12 @@
+// JavaScript for Hamburger
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector("#primary-nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  hamburger.textContent = nav.classList.contains("open") ? "✖" : "☰";
+});
+
 // Dynamic Copyright Year
 const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
@@ -5,12 +14,3 @@ document.getElementById("currentyear").textContent = year;
 // Last Modified Date
 document.getElementById("lastModified").textContent =
   "Last Modified: " + document.lastModified;
-
-
-// Toggle mobile nav
-const menuButton = document.querySelector("#menuButton");
-const nav = document.querySelector("#mainNav");
-
-menuButton.addEventListener("click", () => {
-  nav.classList.toggle("show");
-});
