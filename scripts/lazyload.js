5cb863a717204db30771bda6cd3207c38eb20140
John Mark <asoyamary@gmail.com>
2026-03-25 15:50:51 +0100

    Lazy Loading Photo Gallery

---
+++ scripts/lazyload.js
@@ -0,0 +1,5 @@
+// Current Year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Last Modified Date
+document.getElementById("lastModified").textContent = document.lastModified;
